d0feca4baacfb37f09a31464ac7218105ed2ddb3
Peter Blunden <peter.blunden@gmail.com>
2024-03-28 21:34:34 +1030

    BlackDiamond Css only version

--- NR-black-diamond-vis/main.js
+++ NR-black-diamond-vis/main.js
@@ -1,71 +1,63 @@
-console.log('Hello');
-
-const SPOKE_INTERVAL = 1000;
+const SPOKE_INTERVAL = 1400;
 const SPOKE_LIFETIME = 17000;
 
-document.addEventListener('DOMContentLoaded', function () {
-    canvas = document.getElementById('mainCanvas');
-    // canvas.width = window.innerWidth;
-    // canvas.height = window.innerHeight;
-    // let ctx = canvas.getContext("2d");
-    // ctx.fillStyle = "black";
-    // ctx.fillRect(0, 0, canvas.width, canvas.height);
-    createSpoke();
-    // setInterval(drawScene, 1000 / 60);
-    mainLoop();
+let stage;
+document.addEventListener("DOMContentLoaded", function () {
+  stage = document.getElementById("stage");
+  createSpoke();
+  mainLoop();
 });
 
-const sourceImages = ['images/spoke_big_01.png', 'images/spoke_big_02.png'];
+const sourceImages = ["images/spoke_big_01.png", "images/spoke_big_02.png"];
 
 const spokes = [];
 const startTime = Date.now();
 let lastSpokeCreated = 0;
 function mainLoop() {
-    const timeSinceLastSpoke = Date.now() - lastSpokeCreated;
-    // console.log(timeSinceLastSpoke);
-    if (timeSinceLastSpoke > SPOKE_INTERVAL) {
-        createSpoke();
-    }
-    requestAnimationFrame(mainLoop);
+  const timeSinceLastSpoke = Date.now() - lastSpokeCreated;
+  // console.log(timeSinceLastSpoke);
+  if (timeSinceLastSpoke > SPOKE_INTERVAL) {
+    createSpoke();
+  }
+  requestAnimationFrame(mainLoop);
 }
 
 function createSpoke() {
-    console.log('createSpoke');
-    const image = new Image();
-    const imageIndex = Math.floor(Math.random() * sourceImages.length);
-    image.src = sourceImages[imageIndex];
+  const image = new Image();
+  const imageIndex = Math.floor(Math.random() * sourceImages.length);
+  image.src = sourceImages[imageIndex];
 
-    const rotate = Math.random() * 360;
-    image.style.rotate = `${rotate}deg`;
-    image.onload = function () {
-        const spoke = {
-            image,
-            createdAt: Date.now(),
-        };
-        document.body.prepend(image);
-        image.className = 'spokeImage';
-        spokes.push(spoke);
-        console.log(spoke);
+  const rotate = Math.random() * 360;
+  image.style.rotate = `${rotate}deg`;
+  image.onload = function () {
+    const spoke = {
+      image,
+      createdAt: Date.now(),
     };
+    stage.prepend(image);
+    image.className = "spokeImage";
+    spokes.push(spoke);
+    console.log(spoke);
+  };
 
-    // trim completed spokes
-    for (let i = 0; i < spokes.length; i++) {
-        const spoke = spokes[i];
-        const age = Date.now() - spoke.createdAt;
-        if (age > SPOKE_LIFETIME) {
-            console.log('removing spoke with age', age);
-            spokes.splice(i, 1);
-            i--;
-        }
+  // trim completed spokes
+  for (let i = 0; i < spokes.length; i++) {
+    const spoke = spokes[i];
+    const age = Date.now() - spoke.createdAt;
+    if (age > SPOKE_LIFETIME) {
+      console.log("removing spoke with age", age);
+      spokes.splice(i, 1);
+      i--;
     }
+  }
 
-    // z-sorting
-    // spokes.forEach((s, i) => {
-    //     debugger;
-    //     s.image.style.zIndex = spokes.length - i;
-    // });
+  // z-sorting
+  // spokes.forEach((s, i) => {
+  //     debugger;
+  //     s.image.style.zIndex = spokes.length - i;
+  // });
 
-    lastSpokeCreated = Date.now();
+  lastSpokeCreated = Date.now();
 }
 
 // function drawScene() {
